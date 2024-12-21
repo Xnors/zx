8c392faaaeaeba826720a4b14cd8f7c7962a6e0a
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-12-21 19:55:52 +0800

    完成布局模板

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 周夏-个人博客网页
 
 立项报告: [点击查看](./周夏-个人博客网页开发-项目立项报告.md)
+访问地址: [https://xnors.us.kg/zx](https://xnors.us.kg/zx)
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
-
-
   return (
     <>
-      周夏子朗的个人博客
+      <div class="sidebar">
+        <div className="zx-photo">我周夏哥的帅照</div>
+        <div className="self-intro">
+          自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍自我介绍
+        </div>
+      </div>
+      <div class="main-content">
+        <h1>主要内容区域</h1>
+        <p>这里是网页的主要内容...</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
