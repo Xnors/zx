08d1833d7360a63f962589b2b119a12264b1d664
fxbd5684 <136948328+fxbd5684@users.noreply.github.com>
2024-12-27 18:18:21 +0800

    尝试修复路由不显示的BUG

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,22 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.jsx'
-import { Router } from "@solidjs/router";
+import { render } from "solid-js/web";
+import "./index.css";
+import { Router, Route } from "@solidjs/router";
 
+import Home from "./Home";
+import Test0 from "./blogs/Test0";
+// render(() => <Router />, document.getElementById("root"));
 
-const root = document.getElementById('root')
-
-render(() => <Router />, root)
+function NotFound() {
+  return <h1>404 Not Found</h1>;
+}
+render(
+  () => (
+    <Router>
+      <Route path="/blogs/test" component={Test0} />
+      <Route path="/" component={Home} />
+      <Route path="*404" component={NotFound} />
+    </Router>
+  ),
+  document.getElementById("root")
+);
